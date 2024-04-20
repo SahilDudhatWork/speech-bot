2860c4ac5119ff1d61be06f4da4b82bbcad5f42b
Sahil Dudhat <sahildudhat03@gmail.com>
2024-04-20 15:58:15 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <router-view />
+</template>
+
+
+<script>
+// import { mapMutations } from 'vuex';
+// import MyNavbar from '@/components/MyNavbar.vue'
+
+
+export default {
+  name: 'App',
+  // components: {
+  //   MyNavbar,
+  // },
+}
+</script>
+<style></style>
+
+
